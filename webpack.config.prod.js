e49b9a4b9e04b01187783e40c76a7eae304783a2
RaidenDilan <raiden18@me.com>
2020-04-13 14:36:35 +0100

    Production Workflow & Wrap Up

---
+++ webpack.config.prod.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // location of generated output
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  devtool: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        // multiple loaders
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              indent: 'postcss',
+              // execute plugins
+              plugins: () => [autoprefixer()]
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(png|jp?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]' // limit=8000 kilobytes, path it should be stored it.
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html', // base html file
+      filename: 'index.html', // generate the file
+      inject: 'body' // inject content into 'head' or 'body'
+    })
+  ]
+};
